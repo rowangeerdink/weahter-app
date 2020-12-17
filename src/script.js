b78d4d68cce517aa5a1d6fcc713b44b934b5ce89
rowangeerdink <76164581+rowangeerdink@users.noreply.github.com>
2020-12-17 14:05:22 +0100

    created file: index, script, styles

---
+++ src/script.js
@@ -0,0 +1,68 @@
+let now = new Date();
+let dateTimeToday = document.querySelector("#date-time");
+
+let date = now.getDate();
+let time = now.getHours() + ":" + now.getMinutes();
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+
+dateTimeToday.innerHTML = `${day} ${month} ${date} ${time}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "12087b5c6e656cb621cae20a854dfb64";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+}
+
+function searcLocation(position) {
+  //let latitude = position.coords.latitude;
+  //let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "12087b5c6e656cb621cae20a854dfb64";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searcLocation);
+}
+
+let searchForm = document.querySelector("#search-city-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Haarlem");
